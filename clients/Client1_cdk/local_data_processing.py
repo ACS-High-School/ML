2fb8c4c03b9ed8ce7fc81b62ca180669584907c0
dayoungjung <skswjdekdud1103@gmail.com>
2024-03-06 19:25:15 +0900

    AHSK-234 feat : 전처리 프로세스 변경

--- clients/Client1_cdk/local_data_processing.py
+++ clients/Client1_cdk/local_data_processing.py
@@ -1,38 +1,32 @@
 import numpy as np
-import tensorflow as tf
+import pandas as pd
 import os
 
-def input_fn(member_ID, num_train_per_client, num_test_per_client):
-    getLocalSamples(member_ID, num_train_per_client, num_test_per_client)
-    x_train_client = np.load('local_dataset/x_train_client.npy')
-    y_train_client = np.load('local_dataset/y_train_client.npy')
-    x_test_client = np.load('local_dataset/x_test_client.npy')
-    y_test_client = np.load('local_dataset/y_test_client.npy')
-   
+from sklearn.preprocessing import StandardScaler, LabelEncoder
+from sklearn.model_selection import train_test_split
+
+
+def input_fn(member_ID, file_path, test_size_per_client):
+    train_dataframe = pd.read_csv(file_path)
+    pre_train = data_preprocess(train_dataframe)
+    x_data = pre_train.drop('Exited', axis=1)
+    y_data = pre_train['Exited']
+    x_train_client, x_test_client, y_train_client, y_test_client = train_test_split(x_data,
+                                                                                    y_data,
+                                                                                    test_size=test_size_per_client)
+
     return x_train_client, y_train_client, x_test_client, y_test_client
 
-def getLocalSamples(clientId, num_train_per_client, num_test_per_client):
-    
-    x_train_file = 'local_dataset/x_train_client.npy'
-    y_train_file = 'local_dataset/y_train_client.npy'
-    x_test_file = 'local_dataset/x_test_client.npy'
-    y_test_file = 'local_dataset/y_test_client.npy'
-    
-    if (os.path.exists(x_train_file) and 
-        os.path.exists(y_train_file) and
-        os.path.exists(x_test_file) and
-        os.path.exists(y_test_file)):
-        return    
-    
-    mnist = tf.keras.datasets.mnist
-    (x_train, y_train), (x_test, y_test) = mnist.load_data()
-    x_train_client = x_train[(clientId -1)*num_train_per_client:clientId*num_train_per_client] / 255.0
-    y_train_client = y_train[(clientId -1)*num_train_per_client:clientId*num_train_per_client]
 
-    x_test_client = x_test[(clientId -1)*num_test_per_client:clientId*num_test_per_client] / 255.0
-    y_test_client = y_test[(clientId -1)*num_test_per_client:clientId*num_test_per_client]
-    
-    np.save('local_dataset/x_train_client.npy', x_train_client, allow_pickle=True)
-    np.save('local_dataset/y_train_client.npy', y_train_client, allow_pickle=True)
-    np.save('local_dataset/x_test_client.npy', x_test_client, allow_pickle=True)
-    np.save('local_dataset/y_test_client.npy', y_test_client, allow_pickle=True)
+def data_preprocess(dataframe):
+    label_encoder = LabelEncoder()
+    dataframe['Geography'] = label_encoder.fit_transform(dataframe['Geography'])
+    dataframe['Gender'] = label_encoder.fit_transform(dataframe['Gender'])
+    dataframe['Surname'] = label_encoder.fit_transform(dataframe['Surname'])
+
+    num_features = ['CustomerId', 'CreditScore', 'Age', 'Tenure', 'Balance', 'NumOfProducts', 'HasCrCard',
+                    'IsActiveMember', 'EstimatedSalary']
+    scaler = StandardScaler()
+    dataframe[num_features] = scaler.fit_transform(dataframe[num_features])
+
+    return dataframe
